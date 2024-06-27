f8bfc4dceb7b00af7a1fea51625029208c743930
Guilherme Barrinha <guilhermebarrinhams@gmail.com>
2024-06-27 16:07:56 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -2,7 +2,9 @@ package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan (basePackages = "com.educandoweb.course.entities")
 @SpringBootApplication
 public class CourseApplication {
 
